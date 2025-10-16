6d86f2d8a78f23830b417a057cf28e8f10a6de9e
Cathy Qi <cqi@Cathys.local>
2025-10-16 22:33:14 +0100

    bmi function

--- func/func_cal_bmi.R
+++ func/func_cal_bmi.R
@@ -6,10 +6,16 @@
 #' @param unit_is_metric TRUE if using metric units, FALSE if using imperial units
 #' @returns A single value for bmi (in kg/m^2)
 func_cal_bmi <- function(weight, height, unit_is_metric = TRUE) {
-    # implement this function here
+    if(unit_is_metric) {
+      bmi <- weight/(height^2)
+    } else {
+      bmi <- 703 * weight/(height^2)
+    }
+    return(bmi)
 }
 
 if (FALSE) {
     func_cal_bmi(weight = 90, height = 1.75) # expects 29.38776
     func_cal_bmi(weight = 198, height = 69, unit_is_metric = FALSE) # expects 29.23629
 }
+
