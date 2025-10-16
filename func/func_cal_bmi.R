6f4b1221d0e02f9a9f534737b31ecad0080d2690
JZheng1112 <Jcz312606@outlook.com>
2025-10-16 16:47:31 +0100

    HW4

--- func/func_cal_bmi.R
+++ func/func_cal_bmi.R
@@ -5,11 +5,18 @@
 #' @param height The height of the person in m (metric) / inches (imperial)
 #' @param unit_is_metric TRUE if using metric units, FALSE if using imperial units
 #' @returns A single value for bmi (in kg/m^2)
-func_cal_bmi <- function(weight, height, unit_is_metric = TRUE) {
-    # implement this function here
+func_cal_bmi <- function(weight, height, unit_is_metric=TRUE) {
+  if(unit_is_metric){
+    bmi<- weight/height^2
+  }else{
+    bmi<- 703*weight/(height^2)
+  }
+    print(bmi)
 }
 
-if (FALSE) {
-    func_cal_bmi(weight = 90, height = 1.75) # expects 29.38776
-    func_cal_bmi(weight = 198, height = 69, unit_is_metric = FALSE) # expects 29.23629
+if(TRUE){
+  func_cal_bmi(weight = 90, height = 1.75) # expects 29.38776
+  func_cal_bmi(weight = 198, height = 69, unit_is_metric = FALSE) # expects 29.23629
 }
+
+
