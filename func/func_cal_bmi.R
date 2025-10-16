14818ba51f0066b09532d6fc650a60441b072726
“Your_github_username” <canw22868@gmail.com>
2025-10-16 14:25:33 +0100

    loop

--- func/func_cal_bmi.R
+++ func/func_cal_bmi.R
@@ -1,15 +1,20 @@
 #' Calculate BMI given weight and height
 #' (formula ref: https://en.wikipedia.org/wiki/Body_mass_index)
 #' @include none (meaning that this function don't depend on any other functions)
-#' @param weight The weight of the person in kg (metric) / ibs (imperial)
+#' @param weight The weight of the person in kg (metric) / lbs (imperial)
 #' @param height The height of the person in m (metric) / inches (imperial)
 #' @param unit_is_metric TRUE if using metric units, FALSE if using imperial units
 #' @returns A single value for bmi (in kg/m^2)
 func_cal_bmi <- function(weight, height, unit_is_metric = TRUE) {
-    # implement this function here
+  if (unit_is_metric) {
+    bmi <- weight / (height ^ 2)
+  } else {
+    bmi <- (703 * weight) / (height ^ 2)
+  }
+  return(bmi)
 }
 
-if (FALSE) {
-    func_cal_bmi(weight = 90, height = 1.75) # expects 29.38776
-    func_cal_bmi(weight = 198, height = 69, unit_is_metric = FALSE) # expects 29.23629
-}
+
+func_cal_bmi(90, 1.75)
+
+func_cal_bmi(198, 69, FALSE)
