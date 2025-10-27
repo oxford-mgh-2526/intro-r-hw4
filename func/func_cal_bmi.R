552309500e8c6d8e3b36fa05240008380886f146
josephkalibbala <joseph.kalibbala@gtc.ox.ac.uk>
2025-10-27 05:03:43 +0000

    Commit_HW

--- func/func_cal_bmi.R
+++ func/func_cal_bmi.R
@@ -9,9 +9,9 @@ func_cal_bmi <- function(weight, height, unit_is_metric = TRUE) {
     # implement this function here
     
         if (unit_is_metric) {
-            bmi <- weight / (height ^ 2)
+            bmi <- weight/(height ^ 2)
         } else {
-            bmi <- 703 * weight / (height ^ 2)  
+            bmi <- 703 * (weight/(height ^ 2))  
         }
         
         return(bmi)  
